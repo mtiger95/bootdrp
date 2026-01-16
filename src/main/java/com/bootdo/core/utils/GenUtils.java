5638f62255ebbb9951fc2c5e0c61389aabca3931
lbwahoo <lbwahoo@163.com>
2026-01-16 17:30:11 +0800

    移除 velocity；删除quartz JobFactory配置。

--- src/main/java/com/bootdo/core/utils/GenUtils.java
+++ src/main/java/com/bootdo/core/utils/GenUtils.java
@@ -9,9 +9,10 @@ import com.bootdo.modular.system.domain.TableDO;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.Velocity;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import java.io.File;
 import java.io.IOException;
@@ -30,20 +31,20 @@ public class GenUtils {
 
     public static List<String> getTemplates() {
         List<String> templates = new ArrayList<String>();
-        templates.add("templates/system/generator/domain.java.vm");
-        templates.add("templates/system/generator/Dao.java.vm");
-        //templates.add("templates/common/generator/Mapper.java.vm");
-        templates.add("templates/system/generator/Mapper.xml.vm");
-        templates.add("templates/system/generator/Service.java.vm");
-        templates.add("templates/system/generator/ServiceImpl.java.vm");
-        templates.add("templates/system/generator/Controller.java.vm");
-        templates.add("templates/system/generator/list.html.vm");
-        templates.add("templates/system/generator/add.html.vm");
-        templates.add("templates/system/generator/edit.html.vm");
-        templates.add("templates/system/generator/list.js.vm");
-        templates.add("templates/system/generator/add.js.vm");
-        templates.add("templates/system/generator/edit.js.vm");
-        //templates.add("templates/common/generator/menu.sql.vm");
+        templates.add("system/generator/domain.java");
+        templates.add("system/generator/Dao.java");
+        //templates.add("templates/common/generator/Mapper.java");
+        templates.add("system/generator/Mapper.xml");
+        templates.add("system/generator/Service.java");
+        templates.add("system/generator/ServiceImpl.java");
+        templates.add("system/generator/Controller.java");
+        templates.add("system/generator/list.html");
+        templates.add("system/generator/add.html");
+        templates.add("system/generator/edit.html");
+        templates.add("system/generator/list.js");
+        templates.add("system/generator/add.js");
+        templates.add("system/generator/edit.js");
+        //templates.add("templates/common/generator/menu.sql");
         return templates;
     }
 
@@ -94,10 +95,8 @@ public class GenUtils {
             tableDO.setPk(tableDO.getColumns().get(0));
         }
 
-        //设置velocity资源加载器
-        Properties prop = new Properties();
-        prop.put("file.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
-        Velocity.init(prop);
+        //初始化Thymeleaf模板引擎
+        TemplateEngine templateEngine = initTemplateEngine();
 
         //封装模板数据
         Map<String, Object> map = new HashMap<>(16);
@@ -112,15 +111,17 @@ public class GenUtils {
         map.put("author", config.getString("author"));
         map.put("email", config.getString("email"));
         map.put("datetime", DateUtils.format(new Date(), DateUtils.DATE_TIME_FORMAT));
-        VelocityContext context = new VelocityContext(map);
+        map.put("hasBigDecimal", hasBigDecimal(tableDO.getColumns()));
+
+        Context context = new Context();
+        context.setVariables(map);
 
         //获取模板列表
         List<String> templates = getTemplates();
         for (String template : templates) {
             //渲染模板
             StringWriter sw = new StringWriter();
-            Template tpl = Velocity.getTemplate(template, "UTF-8");
-            tpl.merge(context, sw);
+            templateEngine.process(template, context, sw);
 
             try {
                 //添加到zip
@@ -131,6 +132,33 @@ public class GenUtils {
                 throw new BizServiceException("渲染模板失败，表名：" + tableDO.getTableName());
             }
         }
+    }
+
+    /**
+     * 初始化Thymeleaf模板引擎
+     */
+    private static TemplateEngine initTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("templates/");
+        resolver.setSuffix(".vm"); // 保持原有模板文件扩展名不变
+        resolver.setTemplateMode(TemplateMode.TEXT);
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable(false);
+        templateEngine.setTemplateResolver(resolver);
+        return templateEngine;
+    }
+
+    /**
+     * 检查是否包含BigDecimal类型
+     */
+    private static boolean hasBigDecimal(List<ColumnDO> columns) {
+        for (ColumnDO column : columns) {
+            if ("BigDecimal".equals(column.getAttrType())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -175,65 +203,61 @@ public class GenUtils {
             packagePath += packageName.replace(".", File.separator) + File.separator;
         }
 
-        if (template.contains("domain.java.vm")) {
+        if (template.contains("domain.java")) {
             return packagePath + "domain" + File.separator + className + "DO.java";
         }
 
-        if (template.contains("Dao.java.vm")) {
+        if (template.contains("Dao.java")) {
             return packagePath + "dao" + File.separator + className + "Dao.java";
         }
 
-//		if(template.contains("Mapper.java.vm")){
+//		if(template.contains("Mapper.java")){
 //			return packagePath + "dao" + File.separator + className + "Mapper.java";
 //		}
 
-        if (template.contains("Service.java.vm")) {
+        if (template.contains("Service.java")) {
             return packagePath + "service" + File.separator + className + "Service.java";
         }
 
-        if (template.contains("ServiceImpl.java.vm")) {
+        if (template.contains("ServiceImpl.java")) {
             return packagePath + "service" + File.separator + "impl" + File.separator + className + "ServiceImpl.java";
         }
 
-        if (template.contains("Controller.java.vm")) {
+        if (template.contains("Controller.java")) {
             return packagePath + "controller" + File.separator + className + "Controller.java";
         }
 
-        if (template.contains("Mapper.xml.vm")) {
-            return "main" + File.separator + "resources" + File.separator + "mapper" + File.separator + packageName + File.separator + className + "Mapper.xml";
+        if (template.contains("Mapper.xml")) {
+            return "main" + File.separator + "resources" + File.separator + "mybatis" + File.separator + packageName + File.separator + className + "Mapper.xml";
         }
 
-        if (template.contains("list.html.vm")) {
+        if (template.contains("list.html")) {
             return "main" + File.separator + "resources" + File.separator + "templates" + File.separator
                     + packageName + File.separator + classname + File.separator + classname + ".html";
-            //				+ "modules" + File.separator + "generator" + File.separator + className.toLowerCase() + ".html";
+            //			+ "modules" + File.separator + "generator" + File.separator + className.toLowerCase() + ".html";
         }
-        if (template.contains("add.html.vm")) {
+        if (template.contains("add.html")) {
             return "main" + File.separator + "resources" + File.separator + "templates" + File.separator
                     + packageName + File.separator + classname + File.separator + "add.html";
         }
-        if (template.contains("edit.html.vm")) {
+        if (template.contains("edit.html")) {
             return "main" + File.separator + "resources" + File.separator + "templates" + File.separator
                     + packageName + File.separator + classname + File.separator + "edit.html";
         }
 
-        if (template.contains("list.js.vm")) {
+        if (template.contains("list.js")) {
             return "main" + File.separator + "resources" + File.separator + "static" + File.separator + "js" + File.separator
                     + "appjs" + File.separator + packageName + File.separator + classname + File.separator + classname + ".js";
             //		+ "modules" + File.separator + "generator" + File.separator + className.toLowerCase() + ".js";
         }
-        if (template.contains("add.js.vm")) {
+        if (template.contains("add.js")) {
             return "main" + File.separator + "resources" + File.separator + "static" + File.separator + "js" + File.separator
                     + "appjs" + File.separator + packageName + File.separator + classname + File.separator + "add.js";
         }
-        if (template.contains("edit.js.vm")) {
+        if (template.contains("edit.js")) {
             return "main" + File.separator + "resources" + File.separator + "static" + File.separator + "js" + File.separator
                     + "appjs" + File.separator + packageName + File.separator + classname + File.separator + "edit.js";
         }
-
-//		if(template.contains("menu.sql.vm")){
-//			return className.toLowerCase() + "_menu.sql";
-//		}
 
         return null;
     }
